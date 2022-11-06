7853d9a439e7c5aaae8da5105de931b03662fdff
robobait <kerrykj8@yahoo.com>
2022-11-06 11:58:29 -0800

    fetched menu data

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 import Deals from './main_components/Deals'
 import Menu from './main_components/Menu'
@@ -9,25 +10,66 @@ import Home from './main_components/Home'
 import '../index.scss'
 
 export default function Main() {
+
+  // STATES ---
+  const [menuData, setMenuData] = useState(null)
+
+
+  const { REACT_APP_API_MENU_URL } = process.env
+
+  // API ---
+  const fetchMenu = async() => {
+    try {
+      const res = await fetch(REACT_APP_API_MENU_URL)
+      const data = await res.json()
+      setMenuData(data)
+    } catch (err) {
+      console.log('failed to fetch menu')
+    }
+  }
+
+  useEffect(() => {
+    fetchMenu()
+  },[])
+
+
+  // LOADED RETURN ---
+  const loaded = () => {
+    return (
+      <main className='main'>
+        <Routes>
+          <Route path='/' element={
+            <Home menuData = {menuData} />
+          }/>
+          <Route path='/menu' element={
+            <Menu />
+          }/>
+          <Route path='/deals' element={
+            <Deals />
+          }/>
+          <Route path='/rewards' element={
+            <Rewards />
+          }/>
+          <Route path='/cart' element={
+            <Cart />
+          }/>
+        </Routes>
+      </main>
+    )  
+  }
+
+  const loading = () => {
+    return (
+     <main>
+      loading...
+     </main>
+    )
+  }
+
   return (
-    <main className='main'>
-      <Routes>
-        <Route path='/' element={
-          <Home />
-        }/>
-        <Route path='/menu' element={
-          <Menu />
-        }/>
-        <Route path='/deals' element={
-          <Deals />
-        }/>
-        <Route path='/rewards' element={
-          <Rewards />
-        }/>
-        <Route path='/cart' element={
-          <Cart />
-        }/>
-      </Routes>
-    </main>
-  )  
+      <>
+        {menuData ? loaded() : loading()}
+      </>
+    )
+
 }
--- src/components/main_components/Home.jsx
+++ src/components/main_components/Home.jsx
@@ -1,14 +1,18 @@
 import './home.scss'
 
 import { Link } from 'react-router-dom'
+import { useEffect } from 'react'
 
-export default function Home() {
+export default function Home({ menuData }) {
+
+  console.log(menuData[0].name)
+
   return (
     <div className='home'>
       <Link to='/menu' className='router-link home__order-now'>
           Order Now
       </Link>
-      <Link to='/deals/add' className='router-link home__featured-deal'>
+      <Link to='/deals' className='router-link home__featured-deal'>
           <h1>Featurd Deal</h1>
           <h2>Tom's Test Deal</h2>
       </Link>
